52dfff622573d28ccb8299e61212ea00175d342e
faran ataei <faran.ataei1999@gmail.com>
2024-06-21 22:56:36 +0300

    main update 43

--- mongodb-homework-1/db/mongo.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Connect to a Database using Mongoose and a Connection String
-
-const db = async () => {
-  // write your code here
-};
-
-module.exports = { db };
